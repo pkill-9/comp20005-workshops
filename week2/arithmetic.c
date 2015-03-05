0942c597bcab9677b01a4076c43b28911105fcbc
Matthew Signorini <m.signorini@student.unimelb.edu.au>
2015-03-05 12:23:18 +1100

    Started basic slides for first workshop.
    Added skeleton code for a simple worked example of C style IO and
    arithmetic.
    
    Also added a gitignore which covers *all* the files generated by LaTeX.

---
+++ week2/arithmetic.c
@@ -0,0 +1,13 @@
+/**
+ *  This program asks its user to enter two numbers, reads the numbers from
+ *  the terminal, then computes the sum, difference, product and quotient.
+ */
+
+#include <stdio.h>
+
+    int 
+main (int argc, char **argv) 
+{
+}
+
+// vim: ts=4 sw=4 et
