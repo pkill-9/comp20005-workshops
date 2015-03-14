d9660e6ee99fa481aca05d7367f5270165ff09fa
Matthew Signorini <m.signorini@student.unimelb.edu.au>
2015-03-14 16:33:10 +1100

    Fixed the error causing the program to issue short change by 1 cent. We sometimes need to have two 2 cent coins in the list.
    Also changed the constant names to something a little more descriptive; 50
    cents is now COIN50 rather than COIN0.
    
    The test script still shows that there is a bug though.

--- week3/listchange.c
+++ week3/listchange.c
@@ -7,19 +7,19 @@
 #include <stdlib.h>
 
 /** Definition of coin values for Australian currency. */
-#define COIN0       50
-#define COIN1       20
-#define COIN2       10
-#define COIN3       5
-#define COIN4       2
-#define COIN5       1
+#define COIN50       50
+#define COIN20       20
+#define COIN10       10
+#define COIN5       5
+#define COIN2       2
+#define COIN1       1
 
     int
 main (int argc, char **argv)
 {
     int balance;
 
-    printf ("Enter a value in cents: ");
+    printf ("Enter a value in cents:\n");
 
     /** get the ammount of money from the user, and abort the program on
      *  non integer input. */
@@ -38,40 +38,47 @@ main (int argc, char **argv)
     /** This will be a long sequence of if constructs. We will start with
      *  subtracting the largest coin from the balance then move on to
      *  smaller coins. */
-    if (balance >= COIN0)
+    if (balance >= COIN50)
     {
-        printf ("%d, ", COIN0);
-        balance -= COIN0;
+        printf ("%d, ", COIN50);
+        balance -= COIN50;
     }
 
-    if (balance >= COIN1)
+    if (balance >= COIN20)
     {
-        printf ("%d, ", COIN1);
-        balance -= COIN1;
+        printf ("%d, ", COIN20);
+        balance -= COIN20;
     }
 
-    if (balance >= COIN2)
+    if (balance >= COIN10)
     {
-        printf ("%d, ", COIN2);
-        balance -= COIN2;
-    }
-
-    if (balance >= COIN3)
-    {
-        printf ("%d, ", COIN3);
-        balance -= COIN3;
-    }
-
-    if (balance >= COIN4)
-    {
-        printf ("%d, ", COIN4);
-        balance -= COIN4;
+        printf ("%d, ", COIN10);
+        balance -= COIN10;
     }
 
     if (balance >= COIN5)
     {
         printf ("%d, ", COIN5);
         balance -= COIN5;
+    }
+
+    if (balance >= COIN2)
+    {
+        printf ("%d, ", COIN2);
+        balance -= COIN2;
+
+        /** Sometimes (eg for 4 cents) we will need two 2 cent coins. */
+        if (balance >= COIN2)
+        {
+            printf ("%d, ", COIN2);
+            balance -= COIN2;
+        }
+    }
+
+    if (balance >= COIN1)
+    {
+        printf ("%d, ", COIN1);
+        balance -= COIN1;
     }
 
     /** Check that we have provided coins adding up to the value that the
