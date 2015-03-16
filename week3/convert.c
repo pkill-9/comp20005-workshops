7eb20da1ef044a62edbc8df369b777040869fa79
Matthew Signorini <m.signorini@student.unimelb.edu.au>
2015-03-16 12:28:35 +1100

    Output of convert now limited to 3 decimal places. Also, convert now allows units to be in either upper case or lower case; the char is turned into upper case with the toupper() function.

--- week3/convert.c
+++ week3/convert.c
@@ -11,6 +11,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
     int
 main (int argc, char **argv)
@@ -18,42 +19,44 @@ main (int argc, char **argv)
     double input;
     char units;
 
+    printf ("Enter a quantity:\n");
+
     if (scanf ("%lf%c", &input, &units) != 2)
     {
         printf ("Enter a number followed by a unit\n");
         exit (1);
     }
 
-    switch (units)
+    switch (toupper (units))
     {
     case 'C':
         /** Celsius to Farenheit */
-        printf ("%f F\n", input * 9 / 5 + 32);
+        printf ("%.3f F\n", input * 9 / 5 + 32);
         break;
 
     case 'F':
         /** Farenheit to Celsius. Careful of integer division here */
-        printf ("%f C\n", 5.0 * (input - 32) / 9);
+        printf ("%.3f C\n", 5.0 * (input - 32) / 9);
         break;
 
     case 'M':
         /** Miles to kilometers */
-        printf ("%f km\n", input * 1.609);
+        printf ("%.3f km\n", input * 1.609);
         break;
 
     case 'K':
         /** kilometers to miles */
-        printf ("%f miles\n", input / 1.609);
+        printf ("%.3f miles\n", input / 1.609);
         break;
 
     case 'P':
         /** pounds to kilograms */
-        printf ("%f kg\n", input * 0.454);
+        printf ("%.3f kg\n", input * 0.454);
         break;
 
     case 'G':
         /** kilograms to pounds */
-        printf ("%f lbs\n", input / 0.454);
+        printf ("%.3f lbs\n", input / 0.454);
         break;
 
     default:
