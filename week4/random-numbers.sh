06a98b0de21de0772fcd71cd33dcb0528a2335dd
Matthew Signorini <m.signorini@student.unimelb.edu.au>
2015-03-18 10:23:54 +1100

    Modified random numbers script so that it takes the upper and lower limits of the numbers to generate as command line params. Also added a comment to describe how to use the script.

--- week4/random-numbers.sh
+++ week4/random-numbers.sh
@@ -1,8 +1,18 @@
 #!  /bin/bash
+#
+# Script to generate some random numbers. It should have 3 command line
+# arguments: the first two are the lower and upper limits of the numbers
+# to generate, and the third is the number of randoms to be generated.
+#
+# eg: ./random-numbers.sh 4 107 50
+# 
+# will generate 50 random numbers between 4 and 107 inclusive.
 
-for i in `seq 1 $1`
+
+for i in `seq 1 $3`
 do
-    echo "$RANDOM $2 % p" | dc
+    # you are not expected to understand this.
+    echo "$RANDOM $2 $1 - % $1 + p" | dc
 done
 
 # vim: ts=4 sw=4 et
