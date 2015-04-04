71d9856f50847f0f1680ea282bde6540c173b8b0
Matthew Signorini <m.signorini@student.unimelb.edu.au>
2015-04-04 12:59:03 +1100

    Separate function to do stage 1. Uses pointers to store perimeter, area id and nvals of first polygon into local variables in main.

--- project1/polygons.c
+++ project1/polygons.c
@@ -10,6 +10,7 @@
 
 /**********************************************************/
 
+void do_stage_1 (double *perimeter, double *area, int *id, int *nvals);
 double distance_between (double x1, double y1, double x2, double y2);
 
 /**********************************************************/
@@ -17,32 +18,54 @@ double distance_between (double x1, double y1, double x2, double y2);
     int
 main (int argc, char **argv)
 {
+    double perimeter, area;
+    int polygon_id, num_vertices;
+
+    do_stage_1 (&perimeter, &area, &polygon_id, &num_vertices);
+
+    return 0;
+}
+
+/**********************************************************/
+
+/**
+ *  This function is responsible for stage 1 of the project. It will
+ *  print out the points and calculate perimeter and area of the first
+ *  polygon. The perimeter, area, id and number of vertices will also
+ *  be stored in the variables pointed to by this functions parameters.
+ *
+ *  Yay, pointers 3:)
+ */
+    void
+do_stage_1 (double *perimeter, double *area, int *id, int *nvals)
+{
+    int i;
+
     /** coordinates of the first vertex. */
     double xi, yi;
 
     /** coordinates of two end points of an edge of the polygon */
     double x1, y1, x2, y2;
-    double perimeter = 0;
-    int i, polygon_id, num_vertices;
+
+    *perimeter = 0;
+    *area = 0;
+
+    scanf ("%d %d %lf %lf", nvals, id, &xi, &yi);
 
     /** Stage 1. Read the first polygon and calculate the perimeter and
      *  area. */
     printf ("Stage 1\n=======\n");
-    scanf ("%d %d", &num_vertices, &polygon_id);
-
-    printf ("First polygon is %d\n", polygon_id);
-
-    scanf ("%lf %lf", &xi, &yi);
+    printf ("First polygon is %d\n", *id);
     printf ("   x_val    y_val\n");
     printf ("%4.1f %4.1f\n", xi, yi);
     x1 = xi;
     y1 = yi;
 
-    for (i = 1; i < num_vertices; i ++)
+    for (i = 1; i < *nvals; i ++)
     {
         scanf ("%lf %lf", &x2, &y2);
         printf ("%4.1f %4.1f\n", x2, y2);
-        perimeter += distance_between (x1, y1, x2, y2);
+        *perimeter += distance_between (x1, y1, x2, y2);
 
         /** endpoint of the current edge section becomes the start point
          *  of the next section. */
@@ -52,13 +75,11 @@ main (int argc, char **argv)
 
     /** Final edge section is from the final vertex back to the initial
      *  vertex, closing the loop of the perimeter. */
-    perimeter += distance_between (x2, y2, xi, yi);
+    *perimeter += distance_between (x2, y2, xi, yi);
 
-    printf ("perimeter      = %2.2f m\n", perimeter);
+    printf ("perimeter      = %2.2f m\n", *perimeter);
     printf ("area           = unknown m^2\n");
     printf ("eccentricity   = unknown\n");
-
-    return 0;
 }
 
 /**********************************************************/
