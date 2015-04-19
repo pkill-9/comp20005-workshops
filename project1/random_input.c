1d9bbdbaab43d85f7cda0ed912dc85cb69738dc7
Matthew Signorini <m.signorini@student.unimelb.edu.au>
2015-04-19 18:05:06 +1000

    Random input generator works in that it doesn't crash or fail to compile, but some of the polygons it comes up with do not seem to be clockwise.
    I think it might be more worthwile to hand generate some test cases...

--- project1/random_input.c
+++ project1/random_input.c
@@ -11,20 +11,26 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define MAX_POINTS      100
+
 /**********************************************************/
 
 int random_int_between (int lower, int upper);
 void print_random_polygon (void);
+void random_points (double *x, double *y, int nr_points);
+int is_clockwise (double *x, double *y, int nr_points);
+double random_double (double lower, double upper);
 
 /**********************************************************/
 
     int
 main (int argc, char **argv)
 {
-    int nr_polygons;
+    int nr_polygons, i;
 
     /** seed the random number generator with the current time. */
-    srand (time ());
+    srand (time (NULL));
+    srand48 ((long int) time (NULL));
 
     if (argc > 1)
     {
@@ -55,15 +61,63 @@ main (int argc, char **argv)
     void
 print_random_polygon (void)
 {
-    int nr_points, id;
+    int nr_points, id, i;
     double x [MAX_POINTS], y [MAX_POINTS];
 
-    nr_points = random_int_between (2, MAX_POINTS);
+    nr_points = random_int_between (2, 10);
     id = rand ();
 
     do
+    {
         random_points (x, y, nr_points);
+    }
     while (!is_clockwise (x, y, nr_points));
+
+    printf ("%d %d  ", nr_points, id);
+
+    for (i = 0; i < nr_points; i ++)
+        printf ("%f %f  ", x [i], y [i]);
+
+    printf ("\n");
+}
+
+/**********************************************************/
+
+/**
+ *  Generates the specified number of coordinates at random and stores them
+ *  in the two respective arrays.
+ */
+    void
+random_points (double *x, double *y, int nr_points)
+{
+    int i;
+
+    for (i = 0; i < nr_points; i ++)
+    {
+        x [i] = random_double (-10, 10);
+        y [i] = random_double (-10, 10);
+    }
+}
+
+/**********************************************************/
+
+/**
+ *  Test if the points in the x and y arrays traverse the perimeter of
+ *  the polygon in a clockwise direction. Return value is 1 if they are
+ *  clockwise, 0 otherwise.
+ */
+    int
+is_clockwise (double *x, double *y, int nr_points)
+{
+    double total = 0;
+    int i;
+
+    for (i = 1; i < nr_points; i ++)
+        total += (x [i] - x [i - 1]) * (y [i] + y [i - 1]);
+
+    total += (x [i - 1] - x [0]) * (y [i - 1] + y [0]);
+
+    return (total > 0)? 1 : 0;
 }
 
 /**********************************************************/
@@ -83,6 +137,26 @@ random_int_between (int lower, int upper)
     r += lower;
 
     return r;
+}
+
+/**********************************************************/
+
+/**
+ *  Returns a randomly generated double from the specified range, inclusive
+ *  of the endpoints.
+ */
+    double
+random_double (double lower, double upper)
+{
+    double d = drand48 ();
+
+    /** same sort of thing as for random int. Note that since drand48
+     *  returns a value between 0 and 1, we use multiplication rather
+     *  than modulo division to map d to the range. */
+    d *= upper - lower;
+    d += lower;
+
+    return d;
 }
 
 /**********************************************************/
