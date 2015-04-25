3248047d16445e325fba29f21576b91746ac14b8
Matthew Signorini <m.signorini@student.unimelb.edu.au>
2015-04-25 15:46:33 +1000

    Added a function to test if all the items in an array are equal to zero.

--- week7/all_zero.c
+++ week7/all_zero.c
@@ -9,6 +9,15 @@ int all_zero (int a[], int n);
     int
 all_zero (int a[], int n)
 {
+    int i;
+
+    for (i = 0; i < n; i ++)
+    {
+        if (a [i] != 0)
+            return 0;
+    }
+
+    return 1;
 }
 
 /** vim: set ts=4 sw=4 et : */
