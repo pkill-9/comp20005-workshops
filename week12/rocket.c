66bb083fed38895131b8d0a751e3b3c219eee145
Matthew Signorini <m.signorini@student.unimelb.edu.au>
2015-05-30 13:32:41 +1000

    Working solution to 9.11
    Fixed a bug in the drag function; it would return nan if velocity was zero,
    because of a 0/0 error when I tried to get the sign of the velocity. Fixed
    by explicitly testing if the velocity is positive or negative.
    
    Also added functions for thrust force, acceleration, and a constant for
    gravitational acceleration. Constants also have their units shown next to
    them.
    
    Added the rocket simulator to the makefile.

--- week12/rocket.c
+++ week12/rocket.c
@@ -21,15 +21,75 @@
 #include <stdio.h>
 #include <math.h>
 
-#define DRAG_COEFFICIENT        0.6L
-#define FRONTAL_AREA            0.1L
+#define G                       9.81L       /** m/s^2 */
+#define DRAG_COEFFICIENT        0.6L        /** kg/m^3 */
+#define FRONTAL_AREA            0.1L        /** m^2 */
+#define STEP_SIZE               0.001L      /** s */
 
 /**********************************************************/
 
+double thrust (double time);
 double mass (double time);
 double dmdt (double time);
 double drag (double velocity);
-double acceleration (double velocity);
+double acceleration (double velocity, double time);
+
+/**********************************************************/
+
+    int
+main (int argc, char **argv)
+{
+    double time = 0;
+    double velocity = 0, height = 0;
+    double max_height = 0;
+    double a;
+
+    while (height >= 0)
+    {
+        /** get the acceleration. */
+        a = acceleration (velocity, time);
+
+        /** new velocity = old velocity + acceleration * time */
+        velocity += a * STEP_SIZE;
+        height += velocity * STEP_SIZE;
+
+        if (height > max_height)
+            max_height = height;
+
+        time += STEP_SIZE;
+    }
+
+    printf ("Maximum altitude: %10.3f m\n", max_height);
+
+    return 0;
+}
+
+/**********************************************************/
+
+/**
+ *  Calculate the acceleration at the given time, and given the velocity.
+ */
+    double
+acceleration (double velocity, double time)
+{
+    double m = mass (time);
+    return thrust (time) / m - G - drag (velocity) / m - (velocity / m) *
+        dmdt (time);
+}
+
+/**********************************************************/
+
+/**
+ *  Returns the thrust acting on the rocket at a given time.
+ */
+    double
+thrust (double time)
+{
+    if (time <= 10.0)
+        return 500.0;
+
+    return 0.0;
+}
 
 /**********************************************************/
 
@@ -40,8 +100,9 @@ double acceleration (double velocity);
     double
 drag (double velocity)
 {
-    return DRAG_COEFFICIENT * FRONTAL_AREA * fabs (velocity) * 
-        fabs (velocity) * -1 * (velocity / fabs (velocity));
+    double sign = (velocity >= 0)? 1 : -1;
+    return sign * DRAG_COEFFICIENT * FRONTAL_AREA * fabs (velocity) *
+        fabs (velocity);
 }
 
 /**********************************************************/
